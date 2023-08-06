4fe33efc25e4b24f1bd1fa08624896a6b1dcfd6e
AslanSN <alan.staub@hotmail.com>
2023-08-06 13:39:37 +0200

    firsts steps

---
+++ src/sections/dashboard/Dasboard.tsx
@@ -0,0 +1,20 @@
+import { config } from "../../devdash_config";
+
+export const Dashboard = () => {
+	const title = "DevDash_";
+
+	return (
+		<>
+			<header>
+				<h1>{title}</h1>
+			</header>
+			<section>
+				<ul>
+					{config.widgets.map((widget) => (
+						<li key={widget.id}>{widget.repository_url}</li>
+					))}
+				</ul>
+			</section>
+		</>
+	);
+};
